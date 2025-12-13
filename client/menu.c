7a4523af374ef6a5f8f46ea4292e115fc49a0bc9
LibretroAdmin <105389611+LibretroAdmin@users.noreply.github.com>
2025-12-13 14:13:25 +0100

    Update menu.c

--- client/menu.c
+++ client/menu.c
@@ -20,6 +20,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <libretro_file.h>
 
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #ifdef _WIN32
 #include <io.h>
